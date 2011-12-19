3e4bb8022dab3d19acdd96d0b89418864b7701f5
Daniel Montgomery <demonty.montgomery@gmail.com>
2011-12-19 11:47:49 -0600

    first try...

---
+++ demo/index.js
@@ -0,0 +1,7 @@
+var jmath = require("dmonty_mm"); //js lib module
+
+console.log("\n<--begin node app-->\n");
+
+console.log("\nfactorial(5): " + jmath.fac(5));
+
+console.log("\n<--ended okay!-->");
